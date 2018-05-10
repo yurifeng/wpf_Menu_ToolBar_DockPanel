e8baba90c7274ecde1c6c219961b1b0fceb2bd60
yurifeng <yuri_feng@outlook.com>
2018-05-10 10:34:08 +0800

    submit by yurifeng

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace yty_wpf_180509_02
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
